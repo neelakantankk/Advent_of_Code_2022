6a8568ace44a2acb9f5ed1bc164a3d181e098834
Neelakantan K.K <neelakantankk@gmail.com>
2022-12-02 00:09:37 +0530

    Completed Day 01, Part 02

--- day_01/src/lib.rs
+++ day_01/src/lib.rs
@@ -3,10 +3,25 @@ use std::error::Error;
 pub fn run(contents: String) -> Result<(), Box<dyn Error>> {
     let max_cal = part_one(&contents);
     println!("The maximum calories are {}", max_cal);
+
+    let total_cal = part_two(&contents);
+    println!("The total calories are {}", total_cal);
     Ok(())
 }
 
 fn part_one(contents: &str) -> u32 {
+    let elf_calories = get_elf_calories(contents);
+    *elf_calories.iter().max().unwrap()
+}
+
+fn part_two(contents: &str) -> u32 {
+    let mut elf_calories = get_elf_calories(contents);
+    elf_calories.sort();
+    elf_calories.reverse();
+    elf_calories[..3].iter().sum()
+}
+
+fn get_elf_calories(contents: &str) -> Vec<u32> {
     let elves: Vec<_> = contents.split_terminator("\n\n").collect();
     let mut elf_calories = Vec::new();
 
@@ -14,16 +29,14 @@ fn part_one(contents: &str) -> u32 {
         let total_calories: u32 = elf.lines().map(|cal| cal.parse::<u32>().unwrap()).sum();
         elf_calories.push(total_calories);
     }
-
-    *elf_calories.iter().max().unwrap()
+    elf_calories
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_p1() {
+    fn get_test_data() -> &'static str {
         let test_data = "1000
 2000
 3000
@@ -38,6 +51,17 @@ mod tests {
 9000
 
 10000";
+        test_data
+    }
+
+    #[test]
+    fn test_p1() {
+        let test_data = get_test_data();
         assert_eq!(24000, part_one(test_data));
+    }
+    #[test]
+    fn test_p2() {
+        let test_data = get_test_data();
+        assert_eq!(45000, part_two(test_data));
     }
 }
