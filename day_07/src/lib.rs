8298e77537331162afa3316e4d167f34b8024530
Neelakantan K.K <neelakantankk@gmail.com>
2022-12-12 23:53:30 +0530

    Completed Day 07, Part 01

---
+++ day_07/src/lib.rs
@@ -0,0 +1,125 @@
+use std::collections::HashMap;
+use std::error::Error;
+
+#[derive(Debug)]
+enum FileSystemItem {
+    Dir { children: Vec<String> },
+    File { size: usize },
+}
+
+impl FileSystemItem {
+    fn get_size(&self, filesystem: &HashMap<String, FileSystemItem>) -> usize {
+        match self {
+            FileSystemItem::File { size } => *size,
+            FileSystemItem::Dir { children } => {
+                let mut total = 0;
+                for child in children {
+                    let child_item = filesystem.get(&child[..]).unwrap();
+                    total += child_item.get_size(filesystem);
+                }
+                total
+            }
+        }
+    }
+}
+
+pub fn run(contents: String) -> Result<(), Box<dyn Error>> {
+    let filesystem = parse_contents(&contents);
+    let size_of_dirs = get_size_of_dirs(&filesystem);
+    println!(
+        "The total size of directories less than 100,000: {}",
+        size_of_dirs
+    );
+    Ok(())
+}
+
+fn parse_contents(contents: &str) -> HashMap<String, FileSystemItem> {
+    let mut filesystem: HashMap<String, FileSystemItem> = HashMap::new();
+    let mut cwd = String::new();
+    for line in contents.lines() {
+        match line.split_whitespace().next().unwrap() {
+            "$" => match line.split_whitespace().skip(1).collect::<Vec<_>>()[..] {
+                ["cd", ".."] => {
+                    cwd = String::from(cwd.rsplit_once('>').unwrap().0);
+                }
+                ["cd", "/"] => {
+                    cwd = String::from("/");
+                    if !filesystem.contains_key(&cwd[..]) {
+                        filesystem.insert(
+                            cwd.clone(),
+                            FileSystemItem::Dir {
+                                children: Vec::new(),
+                            },
+                        );
+                    }
+                }
+                ["cd", dir_name] => {
+                    let key = format!("{}>{}", cwd, dir_name);
+                    cwd = key.clone();
+                    if !filesystem.contains_key(&key[..]) {
+                        filesystem.insert(
+                            key.clone(),
+                            FileSystemItem::Dir {
+                                children: Vec::new(),
+                            },
+                        );
+                    }
+                }
+                _ => {}
+            },
+            "dir" => {
+                let d_name = line.split_whitespace().nth(1).unwrap();
+                let key = format!("{}>{}", cwd, d_name);
+                if !filesystem.contains_key(&key[..]) {
+                    filesystem.insert(
+                        key.clone(),
+                        FileSystemItem::Dir {
+                            children: Vec::new(),
+                        },
+                    );
+                }
+                add_child(&cwd, d_name, &mut filesystem);
+            }
+            _ => {
+                let f_info = line.split_whitespace().collect::<Vec<_>>();
+                let f_size = f_info[0];
+                let f_name = f_info[1];
+                let key = format!("{}>{}", cwd, f_name);
+                if !filesystem.contains_key(&key[..]) {
+                    filesystem.insert(
+                        key.clone(),
+                        FileSystemItem::File {
+                            size: f_size.parse::<usize>().unwrap(),
+                        },
+                    );
+                }
+                add_child(&cwd, f_name, &mut filesystem);
+            }
+        }
+    }
+    filesystem
+}
+
+fn add_child<'a>(cwd: &str, child_name: &'a str, filesystem: &mut HashMap<String, FileSystemItem>) {
+    let item = filesystem.get_mut(cwd).unwrap();
+    let key = format!("{}>{}", cwd, child_name);
+    if let FileSystemItem::Dir { children, .. } = item {
+        children.push(key);
+    };
+}
+
+fn get_size_of_dirs(filesystem: &HashMap<String, FileSystemItem>) -> usize {
+    let mut total_size = 0;
+    for key in filesystem.keys() {
+        let item = filesystem.get(key).unwrap();
+        let dir_size = if let FileSystemItem::Dir { children: _ } = item {
+            item.get_size(filesystem)
+        } else {
+            0
+        };
+        if dir_size <= 100000 {
+            total_size += dir_size;
+        }
+    }
+    total_size
+}
---
+++ day_07/src/main.rs
@@ -0,0 +1,11 @@
+use std::fs;
+use std::process;
+
+fn main() {
+    let contents = fs::read_to_string("input").expect("Failed to read input file");
+
+    if let Err(e) = day_07::run(contents) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+}
